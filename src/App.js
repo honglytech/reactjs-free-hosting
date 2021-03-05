1c0ab138b926ffd89929e53f2cd356ca12e3e8dd
Hong Ly <lyhong.rupp@gmail.com>
2021-03-05 13:27:03 +1100

    Update App.js
    updated text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          How to Deploy React JS App for FREE
         </p>
         <a
           className="App-link"
